2d855f92110d31210786900b097ba184e6ce2972
Ikram Ghala <ikram.ghala120@gmail.com>
2025-01-29 21:41:04 +0100

    Second commit

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.static('public'));
+//middleware to accept form data (readable)
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 
@@ -39,6 +41,36 @@ app.get('/blogs', (req, res) => {
     console.log(err);
   })
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+  .then((result) => {
+    res.redirect('/blogs');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+  .then((result) => {
+    res.render('details', {blog: result, title: 'Blog Details'});
+  })
+  .catch((err) => console.log(err));
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then(result => {
+    res.json({redirect: '/blogs'});
+  })
+  .catch(err => console.log(err));
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: 'Create Blog'});
